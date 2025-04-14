22fb782661f7fce8e9baae08b0b61e3a56b544f2
OkazakiTakuma <k024g0021@g.neec.ac.jp>
2025-04-15 03:54:05 +0900

    画像、3Dモデル、音声の生成、描画及び再生

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,10 +1,31 @@
 #pragma once
 #include "KamataEngine.h"
 
-
 class GameScene {
 public:
 	void Initialize();
+	~GameScene();
 	void Update();
 	void Draw();
+
+private:
+	// テクスチャーハンドル
+	uint32_t tecstureHandle_ = 0;
+	// スプライト
+	KamataEngine::Sprite* sprite_ = nullptr;
+	// 3Dモデル
+	KamataEngine::Model* model_ = nullptr;
+	// ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+	// カメラ
+	KamataEngine::Camera camera_;
+	// Imguiで値を入力する変数　
+	float inputFloat3[3] = {0, 0, 0};
+	// デバッグカメラ
+	KamataEngine::DebugCamera* debugCamera_ = nullptr;
+
+	// サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+	// 音楽再生ハンドル
+	uint32_t voiceHandle_ = 0;
 };
