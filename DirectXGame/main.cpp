d7acefad9b8c342c148a67ca6792ddde319158fd
OkazakiTakuma <k024g0021@g.neec.ac.jp>
2025-04-14 14:20:44 +0900

    KamataEngineを入れた

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,28 @@
+#include <Windows.h>
+#include "KamataEngine.h"
+#include "GameScene.h"
+
+using namespace KamataEngine;
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	Initialize(L"LD2B_01_オカザキ_タクマ_AL3");
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	while (true) {
+		if (Update()) {	
+			break;
+		}
+
+		gameScene->Update();
+
+		dxCommon->PreDraw();
+
+		gameScene->Draw();
+
+		dxCommon->PostDraw();
+	}
+	Finalize();
+	delete gameScene;
+	return 0;
+}
